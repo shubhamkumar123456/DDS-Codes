fc76bf1c03058a8da7639e591a45d4a5ccb4259f
shubham kumar <107460088+shubhamkumar123456@users.noreply.github.com>
2025-06-28 08:14:33 +0530

    Create sumOfTwoNumbers.c

---
+++ sumOfTwoNumbers.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+  int a = 10;
+  int b = 4;
+  
+  int sum = a+b;
+  printf("sum of a and b = %d",sum);
+
+    return 0;
+}
